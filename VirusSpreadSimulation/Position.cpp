5c8ec0704e55cfe807ef3b4688149cd10f8ecd37
Bertalan Mucsi <berci620@gmail.com>
2025-10-19 14:42:01 +0200

    Position class is working

--- VirusSpreadSimulation/Position.cpp
+++ VirusSpreadSimulation/Position.cpp
@@ -1,2 +1,61 @@
 #include "Position.h"
 
+
+unsigned int Position::maxX = 100;
+unsigned int Position::maxY = 100;
+
+//--------------------------------------------------------------------------
+//Constructors
+
+Position::Position(const unsigned int x, const unsigned int y)
+{
+	this->_x = x;
+	this->_y = y;
+}
+
+
+//--------------------------------------------------------------------------
+//Operators
+
+bool Position::operator==(Position other) const
+{
+	return _x == other._x && _y == other._y;
+}
+
+
+//--------------------------------------------------------------------------
+//Static functions
+
+void Position::SetMaxXY(const unsigned int x, const unsigned int y)
+{
+	maxX = x;
+	maxY = y;
+}
+
+
+//--------------------------------------------------------------------------
+//Neighbors
+
+std::vector<Position> Position::GetNeighbors()
+{
+	std::vector<Position> result;
+	unsigned int xLeft = (_x == 0 ? 0 : _x - 1);
+	unsigned int xRight = (_x == maxX ? maxX : _x + 1);
+	unsigned int yDown = (_y == 0 ? 0 : _y - 1);
+	unsigned int yUp = (_y == maxY ? maxY : _y + 1);
+
+	for (auto horizontal = xLeft; horizontal <= xRight; ++horizontal)
+	{
+		for (auto vertical = yDown; vertical <= yUp; ++vertical)
+		{
+			if (_x == horizontal && _y == vertical)
+			{}
+			else
+			{
+				result.push_back(Position{ horizontal, vertical });
+			}
+		}
+	}
+
+	return result;
+}
--- VirusSpreadSimulation/Position.h
+++ VirusSpreadSimulation/Position.h
@@ -1,9 +1,30 @@
 #pragma once
 
+#include <vector>
+
 class Position
 {
 private: //Variables
-	int x;
-	int y;
+	unsigned int _x;
+	unsigned int _y;
+	static unsigned int maxX;
+	static unsigned int maxY;
+
+
+public:	//Constructors
+	Position() = default;
+	Position(const unsigned int x, const unsigned int y);
+
+
+public: //Operators
+	bool operator==(Position other) const;
+
+
+public:	//Static functions
+	static void SetMaxXY(const unsigned int x, const unsigned int y);
+
+
+public: //Neighbors
+	std::vector<Position> GetNeighbors();
 };
 
