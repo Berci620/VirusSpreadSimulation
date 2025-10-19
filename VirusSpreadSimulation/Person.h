7e3c648415978f40e78d91a34d3bdcdd5346dbfa
Bertalan Mucsi <berci620@gmail.com>
2025-10-19 16:46:00 +0200

    Person class created

---
+++ VirusSpreadSimulation/Person.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Position.h"
+#include "InfectionStatus.h"
+#include "LifeStatus.h"
+
+class Person
+{
+private:	//Variables
+	Position _position;
+	InfectionStatus _infectionStatus;
+	LifeStatus _lifeStatus;
+};
+
